a4af0cdb011857e847f37b73f066a9bc6dc828f9
aledj2 <aledjones@nhs.net>
2022-12-29 18:03:28 +0000

    update to handle multiple pan nos and python3

--- archer_config.py
+++ archer_config.py
@@ -1,5 +1,8 @@
 import os
+
 testing = False
+testing_project = "003_221215_NB552085_0225_AH2J2JAFX5_ADX99999"
+docker = True
 # =====location of input/output files=====
 # root of folder that contains the apps, automate_demultiplexing_logfiles and
 # development_area scripts
@@ -24,7 +27,13 @@ fastq_folder_path = "Data/Intensities/BaseCalls"
 path_to_watch_folder = "/watched/aledjones\@nhs.net/FusionPlexPanSolidTumorv1_0" #folder made by RLH 20210622
 success_statement = "all fastqs downloaded, transferred to server and deleted ok"
 export_environment = "export DX_API_TOKEN=%s" % Nexus_API_Key
-source_command = " source %s" % (os.path.join(resources_root,"resources","dx-toolkit","environment"))
+
+# when testing the script it may be easier to do so without running the docker image. Different paths are required for some inputs in this case
+if docker:
+    source_command = " source %s" % (os.path.join(resources_root,"resources","dx-toolkit","environment"))
+else:
+    source_command = " source %s" % (os.path.join(document_root,"apps","dx-toolkit","environment"))
+
 path_to_manifest_script = os.path.join(resources_root,"resources","dxda_0.5.7","dxda","scripts","create_manifest.py")
 path_to_filter_manifest_script = os.path.join(resources_root,"resources","dxda_0.5.7","dxda","scripts","filter_manifest.py")
 path_to_dx_download_client = os.path.join(resources_root,"resources","dxda_0.5.7","dx-download-agent-linux")
